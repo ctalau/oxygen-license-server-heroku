e079c7c567869eb6a11c8d12ada88cabe90032b2
ctalau <cristitalau@gmail.com>
2016-08-06 14:53:03 +0300

    Made the install script more verbose

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 echo "admin: $ADMIN_PASSWORD,admin" >> passwords.properties 
 echo "user: $USER_PASSWORD,user" >> passwords.properties
+
+echo "Running postinstall script in: " `pwd`
+echo "Generated passwords: "
+cat passwords.properties
